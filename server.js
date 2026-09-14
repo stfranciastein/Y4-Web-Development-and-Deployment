801e836a977fd32a8b5c33ae8b4f8acf939d2e60
Josh Santiago-Francia <N00151146@iadt.ie>
2026-09-14 16:41:58 +0100

    borg

---
+++ server.js
@@ -0,0 +1,28 @@
+import "dotenv/config";
+import express from "express";
+import path from "path";
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+const notes = [
+  { id: 1, text: "Las Gabriella" },
+  { id: 2, text: "Las Patricia" },
+  { id: 3, text: "Las Nicole" },
+  { id: 4, text: "Las Sofia" },
+];
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
