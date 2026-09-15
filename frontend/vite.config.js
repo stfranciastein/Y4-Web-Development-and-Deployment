28fcbfb5af953903ff3ae4e508ee56be48728d56
Josh Santiago-Francia <N00151146@iadt.ie>
2026-09-15 12:38:35 +0100

    note

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // A proxy to forward API requests to the backend server. You can either do this or configure the frontend to directly call the backend URL.
     proxy: {
       "/api": "http://localhost:8000",
     },
